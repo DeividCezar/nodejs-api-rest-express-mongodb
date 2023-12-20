e3af33f9449345adb541837df14a666d9001c0f1
DeividCezar <perspicilum@gmail.com>
2023-12-19 22:00:11 -0300

    Servidor local e Nodemon

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    "/": "Curso de Node",
+    "/livros": "Pagina de livros",
+    "/autores": "Pagina de autores",
+    "/sobre": "Informacoes sobre projeto"
+}
+
 const server = http.createServer((req, resp) => {
     resp.writeHead(200, {'Content-Type': 'text/plain'});
-    resp.end('Curso de Node');
+    resp.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
-    console.log('Servidor escutando em http://localhost:${port}')
+    console.log('Servidor escutando em http://localhost:3000')
 })
